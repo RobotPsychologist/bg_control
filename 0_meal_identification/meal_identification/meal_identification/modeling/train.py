b14da3456157741bd5f949c192c85e5fd4f1b28f
Jonathan Gong <jonathangong2005@gmail.com>
2024-11-20 16:44:40 -0500

    added more models, refactored fittin code, added base metrics provided by sklearn

--- 0_meal_identification/meal_identification/meal_identification/modeling/train.py
+++ 0_meal_identification/meal_identification/meal_identification/modeling/train.py
@@ -4,6 +4,9 @@ from loguru import logger
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from meal_identification.config import MODELS_DIR, PROCESSED_DATA_DIR
+from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
+from sktime.performance_metrics.forecasting import MeanAbsolutePercentageError, MeanSquaredError, MeanAbsoluteError
+
 
 # Model Imports
 from sktime.annotation.hmm_learn import GMMHMM 
@@ -14,6 +17,7 @@ from sktime.annotation.adapters import PyODAnnotator
 from sktime.annotation.lof import SubLOF
 from hmmlearn import hmm # CategoricalHMM and GaussianHMM uses hmm module
 from sktime.annotation.hmm_learn import PoissonHMM 
+from sktime.annotation.hmm_learn import GaussianHMM 
 
 app = typer.Typer()
 
@@ -28,7 +32,12 @@ def main(
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
 
-    def train_model_instance(X, Y, validation_split = 0.2, model="model", hyperparameters = [], n_iter = 100):
+    def train_model_instance(X, Y, model="model", supervised=False, 
+                             validation_split = 0.2, n_iter = 100, 
+                             n_components = 3, n_mix = 3, covariance_type = 'full', 
+                             verbose = True,
+                             period_length = 10, n_cps = 2, n_neighbors = 36,
+                             window_size = 288, init_params="s", random_state=None):
         """
         Train a model on the given data.
 
@@ -43,11 +52,13 @@ def main(
         model : str, optional
             Model to train, by default "model"
 
-        hyperparameters : list, optional
-            Hyperparameters for the model, by default []
-            GMMHMM: [n_components, n_mix, covariance_type]
-            ClaSPSegmentation: [period_length, n_cps]
-            SubLOF: [n_neighbors, window_size]
+        hyperparameters :
+            Hyperparameters for the model
+            GMMHMM: n_components, n_mix, covariance_type
+            ClaSPSegmentation: period_length, n_cps
+            SubLOF: n_neighbors, window_size
+            PoissonHMM: n_components, n_iter, init_params, random_state, verbose
+            GaussianHMM: n_components, covariance_type, n_iter, init_params, random_state, verbose
 
 
 
@@ -64,38 +75,75 @@ def main(
 
             # Implement starprob, covars_prior, means_prior
 
-            model = GMMHMM(n_components=hyperparameters[0], 
-                         n_mix = hyperparameters[1], 
-                         covariance_type=hyperparameters[2], 
+            model = GMMHMM(n_components=n_components, 
+                         n_mix = n_mix, 
+                         covariance_type=covariance_type, 
                          n_iter=n_iter, 
-                         verbose=True)
-            
-            print("Training GMMHMM...")
-            model.fit(X_train, Y_train)
-            print("Modeling training complete.")
-
-            hidden_states_train = model.predict(X_train)
-            hidden_states_test = model.predict(X_val)
+                         init_params=init_params,
+                        random_state=random_state,
+                         verbose=verbose)
 
             # Use metrics made by sktime team when they come out
 
-            return model
-
         elif model == "ClaSPSegmentation":
 
-            model = ClaSPSegmentation(period_length=hyperparameters[0], 
-                                      n_cps=hyperparameters[1])
-
-            return model
+            model = ClaSPSegmentation(period_length=period_length, 
+                                      n_cps=n_cps)
         
         elif model == "SubLOF":
 
-            model = SubLOF(n_neighbors=hyperparameters[0], window_size=hyperparameters[1])
+            model = SubLOF(n_neighbors=n_neighbors, window_size=window_size)
 
         elif model == "PoissonHMM":
-            return
+            model = PoissonHMM(n_components=n_components,   
+                                n_iter=n_iter,
+                                init_params=init_params,
+                                random_state=random_state,
+                                verbose=verbose)
+        
+        elif model == "GaussianHMM":
+
+            model = GaussianHMM(n_components=n_components, 
+                                covariance_type=covariance_type,
+                                n_iter=n_iter,
+                                init_params=init_params,
+                                random_state=random_state,
+                                verbose=verbose)
+
+        if supervised:
+            print("Training supervised " + model + "...")
+            model.fit(X_train, Y_train)
+            print("Modeling training complete.")
+        else:
+            print("Training unsupervised " + model + "...")
+            model.fit(X_train)
+            print("Modeling training complete.")
+
+        hidden_states_train = model.predict(X_train)
+        hidden_states_test = model.predict(X_val)
+
+        train_mae = mean_absolute_error(hidden_states_train, Y_train)
+        train_mse = mean_squared_error(hidden_states_train, Y_train)
+        train_rmse = mean_squared_error(hidden_states_train, Y_train, squared=False)  # squared=False returns RMSE
+        train_mape = mean_absolute_percentage_error(hidden_states_train, Y_train)
+
+        test_mae = mean_absolute_error(hidden_states_test, Y_val)
+        test_mse = mean_squared_error(hidden_states_test, Y_val)
+        test_rmse = mean_squared_error(hidden_states_test, Y_val, squared=False)  # squared=False returns RMSE
+        test_mape = mean_absolute_percentage_error(hidden_states_test, Y_val)
+
+        print(f"MAE for training data: {train_mae}")
+        print(f"MSE for training data: {train_mse}")
+        print(f"RMSE for training data: {train_rmse}")
+        print(f"MAPE for training data: {train_mape}")
+
+        print(f"MAE for test data: {test_mae}")
+        print(f"MSE for test data: {test_mse}")
+        print(f"RMSE for test data: {test_rmse}")
+        print(f"MAPE for test data: {test_mape}")
 
 
+        return model
 
 
         logger.info("Training some model...")
