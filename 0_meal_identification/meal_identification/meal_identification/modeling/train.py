41ec240ea1eb58387a643aa75c71c186a3192bb6
Jonathan Gong <jonathangong2005@gmail.com>
2024-11-19 23:59:15 -0500

    added model instances

--- 0_meal_identification/meal_identification/meal_identification/modeling/train.py
+++ 0_meal_identification/meal_identification/meal_identification/modeling/train.py
@@ -28,13 +28,13 @@ def main(
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
 
-    def train_model(X, Y, validation_split = 0.2, model="model", hyperparameters = []):
+    def train_model_instance(X, Y, validation_split = 0.2, model="model", hyperparameters = [], n_iter = 100):
         """
         Train a model on the given data.
 
         Parameters
         ----------
-        X : pd.DataFrame
+        X : pd.DataFrame    
             Features.
         Y : pd.Series
             Labels.
@@ -42,8 +42,14 @@ def main(
             Fraction of the data to use for validation, by default 0.2
         model : str, optional
             Model to train, by default "model"
+
         hyperparameters : list, optional
             Hyperparameters for the model, by default []
+            GMMHMM: [n_components, n_mix, covariance_type]
+            ClaSPSegmentation: [period_length, n_cps]
+            SubLOF: [n_neighbors, window_size]
+
+
 
         Returns
         -------
@@ -52,14 +58,51 @@ def main(
         """
 
         # Split the data into training and validation sets
-        X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=validation_split)
-        pass
+        X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=validation_split, shuffle=False)
 
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
+        if model == "GMMHMM":
+
+            # Implement starprob, covars_prior, means_prior
+
+            model = GMMHMM(n_components=hyperparameters[0], 
+                         n_mix = hyperparameters[1], 
+                         covariance_type=hyperparameters[2], 
+                         n_iter=n_iter, 
+                         verbose=True)
+            
+            print("Training GMMHMM...")
+            model.fit(X_train, Y_train)
+            print("Modeling training complete.")
+
+            hidden_states_train = model.predict(X_train)
+            hidden_states_test = model.predict(X_val)
+
+            # Use metrics made by sktime team when they come out
+
+            return model
+
+        elif model == "ClaSPSegmentation":
+
+            model = ClaSPSegmentation(period_length=hyperparameters[0], 
+                                      n_cps=hyperparameters[1])
+
+            return model
+        
+        elif model == "SubLOF":
+
+            model = SubLOF(n_neighbors=hyperparameters[0], window_size=hyperparameters[1])
+
+        elif model == "PoissonHMM":
+            return
+
+
+
+
+        logger.info("Training some model...")
+        for i in tqdm(range(10), total=10):
+            if i == 5:
+                logger.info("Something happened for iteration 5.")
+        logger.success("Modeling training complete.")
     # -----------------------------------------
 
 
