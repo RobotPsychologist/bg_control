1f8e64c19e209627c55bbd78db2c2d82cfb1776c
Jonathan Gong <jonathangong2005@gmail.com>
2024-11-27 12:51:01 -0500

    commented out tests

--- 0_meal_identification/meal_identification/meal_identification/modeling/train.py
+++ 0_meal_identification/meal_identification/meal_identification/modeling/train.py
@@ -336,20 +336,20 @@ def main(
             logger.error(f"Error saving model: {e}")
 
         return model
-
-    logger.info("Training some model...")
-
-    data_filename = "2024-11-15_500030__i5mins_d4hrs_c5g_l2hrs_n3.csv"
-    data_path = INTERIM_DATA_DIR / data_filename
-    current_model_path = MODELS_DIR / "model.pkl"
-
-    train_model_instance(
-        model = "GMMHMM", 
-        data_path=data_path,    
-        model_path=current_model_path,  
-        transformer=ScaledLogitTransformer())
-    logger.success("Modeling training complete.")
     # -----------------------------------------
+    # Sample training:
+    # logger.info("Training some model...")
+
+    # data_filename = "2024-11-15_500030__i5mins_d4hrs_c5g_l2hrs_n3.csv"
+    # data_path = INTERIM_DATA_DIR / data_filename
+    # current_model_path = MODELS_DIR / "model.pkl"
+
+    # train_model_instance(
+    #     model = "GMMHMM", 
+    #     data_path=data_path,    
+    #     model_path=current_model_path,  
+    #     transformer=ScaledLogitTransformer())
+    # logger.success("Modeling training complete.")
 
 if __name__ == "__main__":
     app()
